7dfb1e59c990b5615498f317f122b1d1fca3751d
asstella <e0774801@u.nus.edu>
2024-03-22 23:45:05 +0800

    add nav for questions and knowledge graph, edit layouts

--- app.py
+++ app.py
@@ -11,12 +11,40 @@ async def upload_files(q: Q) -> None:
     # TODO: Do word processing for each document and generate knowledge graph
     # TODO: Create graph in database with parsed files
 
+# display MCQ questions
+def question_generator(q: Q):
+    del q.page['knowledge_graph']
+    q.page['question_generator'] = ui.form_card(box='content', items=[
+            ui.text_xl('Questions'),
+    ])
 
-@app('/')
-async def serve(q: Q) -> None:
+# display knowledge graph
+def knowledge_graph(q: Q):
+    del q.page['question_generator']
+    q.page['knowledge_graph'] = ui.form_card(box='content', items=[
+            ui.text_xl('Knowledge Graph'),
+    ])
+
+def init(q: Q):
+    q.page['meta'] = ui.meta_card(box='', title='AcademIQ', theme='nord', layouts=[
+        ui.layout(breakpoint='xs', 
+                  zones=[ui.zone('header', wrap='stretch'),
+                         ui.zone('body', direction=ui.ZoneDirection.ROW, size='1', zones=[
+                                ui.zone('sidebar', size='25%'),
+                                ui.zone('body2', direction=ui.ZoneDirection.COLUMN, zones=[ui.zone('nav'),
+                                    ui.zone('content')])
+                        ])
+                  ])
+    ])
+    q.page['header'] = ui.header_card(
+        box='header',
+        title='AcademIQ',
+        subtitle='Educational tool to empower educators and learners in knowledge acquisition and assessment',
+        image='https://www.svgrepo.com/show/288267/studying-student.svg',
+    )
     q.page['sidebar'] = ui.form_card(
-        box='1 1 3 -1',
-        title='Collections',
+        box='sidebar',
+        title='Upload Files',
         items=[
             ui.file_upload(
                 name='upload_files',
@@ -26,6 +54,26 @@ async def serve(q: Q) -> None:
             ),
         ]
     )
+    q.page['nav'] = ui.tab_card(
+        box='nav',
+        items=[
+            ui.tab(name='question_generator', label='Question Generation'),
+            ui.tab(name='knowledge_graph', label='Knowledge Graph')  
+            ]
+        )
+        
+    q.client.initialized = True
+
+
+@app('/')
+async def serve(q: Q) -> None:
+    if not q.client.initialized:
+        init(q)
+        question_generator(q)
+    elif q.args.question_generator:
+        question_generator(q)
+    elif q.args.knowledge_graph:
+        knowledge_graph(q)
 
     await run_on(q)
     await q.page.save()
