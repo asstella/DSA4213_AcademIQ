5dfde8fae127ad7484c07be90b70a5df52274f33
asstella <e0774801@u.nus.edu>
2024-04-05 20:38:54 +0800

    edit prompt

--- h2ogpt.py
+++ h2ogpt.py
@@ -4,25 +4,30 @@ client = H2OGPTE(
     address='https://h2ogpte.genai.h2o.ai',
     api_key='sk-rWllOURibnB1EzBeMCvWstytoiaDv2xRNa1QilqFPzNVYvkM',
 )
+# give fixed format to ensure that the prompt output always returns in the format we read it
+output_format = "Example output\n Topic: Topic Modelling\n Summary: Topic modeling is a popular natural language processing \
+                technique used to create structured data from a collection of unstructured data. In other words, the technique enables businesses \
+                to learn the hidden semantic patterns portrayed by a text corpus and automatically identify the topics that exist inside it.\n\
+                Topic: In-context Learning\n Summary: In-context learning (ICL) is a technique where task demonstrations are integrated \
+                into the prompt in a natural language format. This approach allows pre-trained LLMs to address new tasks without fine-tuning the model."
 
 def text_summariser(text_content: str):
     summary = client.summarize_content(
         pre_prompt_summary="Summarize the content below.\n",
         text_context_list=[text_content],
-        prompt_summary="summarize the above into a couple of paragraphs and give me a topic for each short paragraph."
+        prompt_summary=f"summarize the above into a couple of paragraphs and give me a topic for each short paragraph.\n {output_format}"
     )
     return summary.content
 
 def get_topic_summary(text_document: str):
     summary = text_summariser(text_document)
-    topics = ["List", "Of", "Topics"] # TODO: remove placeholder with actual topic labels
     lines = summary.split('\n')
 
-    titles = []
+    topics = []
     paragraphs = []
     for line in lines:
-        if line.startswith('Topic:'):
-            titles.append(line.strip())
+        if line.startswith('Topic'):
+            topics.append(line.split(":")[-1].strip())
         else:
             paragraphs.append(line.strip())
 
